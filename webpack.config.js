62c5b16a43eea8ffd2e43d2c506dbcabbb9a1ebb
K.K <gokennsirou@yahoo.co.jp>
2021-04-17 18:35:43 +0900

    [Add] webpack build config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.ts'),
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'lib')
+  },
+  target: 'node',
+  resolve: {
+    extensions: ['.ts'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
