7f1b304a4356343b264b8b03346c9284d65862bd
K-kind <55728594+K-kind@users.noreply.github.com>
2021-04-17 16:56:42 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+  ignorePatterns: ['/dist/**']
+}
