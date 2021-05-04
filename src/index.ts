d0b2a4ddecc47dca6f73409f49c610fd02b95c6a
K.K <gokennsirou@yahoo.co.jp>
2021-05-04 21:48:31 +0900

    [Add] re-exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
-export {
-  generateSvg,
-  generateRect,
-  generateCircle,
-  generateEllipse,
-  generateLine,
-  generatePolyline,
-  generatePolygon
-} from './lib/draw-svg'
-
-export { SvgAttributes } from './types/attributes/svg'
-
-export {
-  RectAttributes,
-  CircleAttributes,
-  EllipseAttributes,
-  LineAttributes,
-  PolylineAttributes,
-  PolygonAttributes,
-  PathAttributes
-} from './types/attributes/shapes'
+export { SVG, SvgAttributes } from './elements/svg'
+export { Animate, AnimateAttributes } from './elements/animation/animate'
+export { AnimateColor, AnimateColorAttributes } from './elements/animation/animate-color'
+export { AnimateMotion, AnimateMotionAttributes } from './elements/animation/animate-motion'
+export { AnimateTransform, AnimateTransformAttributes } from './elements/animation/animate-transform'
+export { MPath, MPathAttributes } from './elements/animation/mpath'
+export { A, AAttributes } from './elements/common/a'
+export { ForeignObject, ForeignObjectAttributes } from './elements/common/forign-object'
+export { SVGImage, SVGImageAttributes } from './elements/common/image'
+export { Marker, MarkerAttributes } from './elements/common/marker'
+export { Mask, MaskAttributes } from './elements/common/mask'
+export { Pattern, PatternAttributes } from './elements/common/pattern'
+export { Script, ScriptAttributes } from './elements/common/script'
+export { SVGSet, SVGSetAttributes } from './elements/common/set'
+export { Stop, StopAttributes } from './elements/common/stop'
+export { SVGStyle, SVGStyleAttributes } from './elements/common/style'
+export { Switch, SwitchAttributes } from './elements/common/switch'
+export { View, ViewAttributes } from './elements/common/view'
+export { Desc, DescAttributes } from './elements/descriptive/desc'
+export { Metadata, MetadataAttributes } from './elements/descriptive/metadata'
+export { Title, TitleAttributes } from './elements/descriptive/title'
+export { Font, FontAttributes } from './elements/font-face/font'
+export { FontFace, FontFaceAttributes } from './elements/font-face/font-face'
+export { FontFaceFormat, FontFaceFormatAttributes } from './elements/font-face/font-face-format'
+export { FontFaceName, FontFaceNameAttributes } from './elements/font-face/font-face-name'
+export { FontFaceSrc, FontFaceSrcAttributes } from './elements/font-face/font-face-src'
+export { FontFaceUri, FontFaceUriAttributes } from './elements/font-face/font-face-uri'
+export { LinearGradient, LinearGradientAttributes } from './elements/gradient/linear-gradient'
+export { RadialGradient, RadialGradientAttributes } from './elements/gradient/radial-gradient'
+export { Circle, CircleAttributes } from './elements/shapes/circle'
+export { Ellipse, EllipseAttributes } from './elements/shapes/ellipse'
+export { Line, LineAttributes } from './elements/shapes/line'
+export { Path, PathAttributes } from './elements/shapes/path'
+export { Polygon, PolygonAttributes } from './elements/shapes/polygon'
+export { Polyline, PolylineAttributes } from './elements/shapes/polyline'
+export { Rect, RectAttributes } from './elements/shapes/rect'
+export { Defs, DefsAttributes } from './elements/structural/defs'
+export { G, GAttributes } from './elements/structural/g'
+export { SVGSymbol, SVGSymbolAttributes } from './elements/structural/symbol'
+export { Use, UseAttributes } from './elements/structural/use'
+export { AltGlyph, AltGlyphAttributes } from './elements/text/alt-glyph'
+export { SVGText, SVGTextAttributes } from './elements/text/text'
+export { TextPath, TextPathAttributes } from './elements/text/text-path'
+export { Tref, TrefAttributes } from './elements/text/tref'
+export { Tspan, TspanAttributes } from './elements/text/tspan'
